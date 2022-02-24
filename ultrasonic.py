48c82a2264ec57c07c94866d636832b28daf0578
erinnwhittyxx <erinnwhittyxx@gmail.com>
2022-02-24 15:41:48 +0000

    Project setup and libraries imported, main.py

---
+++ ultrasonic.py
@@ -0,0 +1,48 @@
+from machine import Pin, Timer
+import pycom
+import time
+
+def calibration(chrono, trigger, echo, led = False):
+    if led:
+        pycom.rgbled(0x7f0000) # red
+    prev_distance = 0
+    distance = getDistance(chrono, trigger, echo)
+    print("calibration distance is {}".format(distance))
+    count = 0
+    while True:
+        prev_distance = distance
+        distance = getDistance(chrono, trigger, echo)
+        while prev_distance == distance:
+            count+=1
+            print("count: {}".format(count))
+            if count > 5:
+                if led:
+                    pycom.rgbled(0x007f00) # green
+                    time.sleep(1.5)
+                    pycom.rgbled(0) # off
+                return distance
+            time.sleep(1)
+            prev_distance = distance
+            distance = getDistance(chrono, trigger, echo)
+        else:
+            count = 0
+
+def getDistance(chrono, trigger, echo):
+    chrono.reset()
+    trigger(1)
+    time.sleep_us(10)
+    trigger(0)
+
+    while echo() == 0:
+        pass
+    chrono.start()
+
+    while echo() == 1:
+        pass
+    chrono.stop()
+
+    distance = chrono.read_us() / 58.0
+    if distance > 400:
+        return -1
+    else:
+        return int(distance)
